02a1453a784af4027a2d2c222dba178133d6d65d
Datascientist27 <agalahonore27@gmailcom>
2024-01-15 11:31:41 +0000

    create user model

--- Api/index.js
+++ Api/index.js
@@ -1,6 +1,24 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config()
+mongoose
+.connect(process.env.MONGO)
+.then(
+    ()=>{
+        console.log("connected successfully to the db");
+    }
+)
+.catch(
+    (err)=>{
+        console.log(err);
+    }
+);
+
 
 const app = express();
 app.listen(3000,()=>{
     console.log("app is running on port 3000")
 });
+
